6a4acbcf0655d15977ee8833543953a71a1e4e73
JohnF <johnfbelland@gmail.com>
2017-05-30 14:05:16 -0400

    Challenges now posted to slack

--- src/app/ladder-ranking/ladder-ranking.component.ts
+++ src/app/ladder-ranking/ladder-ranking.component.ts
@@ -72,7 +72,7 @@ export class LadderRankingComponent implements OnInit {
       });
       player.isChallenged = true;
       player.canChallenge = false;
-      const message = '#' + this.currentPlayer.rank + ' ' + this.currentPlayer.displayName + ' Challenged #' + player.rank + ' ' + player.displayName + 'to a match';
+      const message = '#' + this.currentPlayer.rank + ' ' + this.currentPlayer.displayName + ' Challenged #' + player.rank + ' ' + player.displayName + ' to a match';
       this.slackService.addToSlackMessageQueue({ channel: 'usonly', message: message });
     }
   }
